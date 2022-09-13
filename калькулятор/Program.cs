e3c9a7164d696e52b2b0c03994b5ab3b12315dae
gercherster <54581681+gercherster@users.noreply.github.com>
2022-09-13 13:45:01 +0500

    Добавьте файлы проекта.

---
+++ калькулятор/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace калькулятор
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstvalue, secondvalue;
+            string action;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter first number");
+                firstvalue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter second number");
+                secondvalue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Choose action: '+' '-' '*' '/' '%'");
+                action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "+":
+                        
+                        Console.WriteLine(firstvalue + secondvalue);
+
+                        break;
+
+                    case "-":
+                        Console.WriteLine(firstvalue - secondvalue);
+                        break;
+
+                    case "*":
+                        Console.WriteLine(firstvalue * secondvalue);
+                        break;
+
+                    case "/":
+                        if (secondvalue == 0)
+                        {
+                            Console.WriteLine("Error! Cannot divide by 0 ");
+                        }
+                        else
+                            Console.WriteLine(firstvalue / secondvalue);
+                        break;
+                    case "%":
+                        Console.WriteLine(firstvalue % secondvalue);                        
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown action");                       
+                        break;
+                }
+
+                Console.ReadLine();
+            }   
+
+        }
+    }
+}
